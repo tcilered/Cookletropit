e9afc285b371b6a3435a47ff7ca42335d42da529
Tcilered <thomasx994@gmail.com>
2026-04-24 14:55:22 +1200

    main menu
    main menu with start and exit button that saves and loads a save file saved to individual user data

--- global_data.gd
+++ global_data.gd
@@ -1,13 +1,15 @@
 extends Node
 
 # Changed the extension from .json to .save since it's now binary data
-const SAVE_PATH = "user://savegame.save"
+var save_path = "user://Cookletropitsavegame.save"
+
+
 
 var player_stats = {
 	"health": 100,
 	"gold": 0,
 	"current_level": 1,
-	"last_position": Vector2(150, 300) # This will now save perfectly!
+	"last_position": Vector2(150, 300) 
 }
 
 ###
@@ -15,18 +17,19 @@ var player_stats = {
 ###
 
 func save_game():
-	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	print(save_path)
 	if file:
 		# store_var() takes ANY Godot variable and saves it directly
 		file.store_var(player_stats)
 		print("Game Saved!")
 
 func load_game():
-	if not FileAccess.file_exists(SAVE_PATH):
+	if not FileAccess.file_exists(save_path):
 		print("No save file found.")
 		return
 	
-	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	var file = FileAccess.open(save_path, FileAccess.READ)
 	
 	# get_var() pulls the data out exactly as it was saved
 	var data = file.get_var()
---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+
+func _on_start_btn_pressed() -> void:
+	GlobalData.load_game()
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_quit_btn_pressed() -> void:
+	GlobalData.save_game()
+	get_tree().quit()
