dd9d9e7c5fa34e5bed6ac419d7e8b16eabdd9b0d
Tcilered <thomasx994@gmail.com>
2026-05-15 09:56:31 +1200

    table squares
    Created a system for categorisation of the square based on possible roulette placement to send a signal to main. Needs only duplication to make whole table

--- table_square.gd
+++ table_square.gd
@@ -4,13 +4,32 @@ signal placing_requested(play_type, square_id, global_spawn_pos)
 signal hover_entered(square_id)
 signal hover_exited(square_id)
 signal hover_moved(play_type, square_id, global_pos)
-
 @export var square_id: int = 17
+
+@export_group("Identification")
+@export_enum("red", "black", "green") var square_colour: String = "red"
+
+@export_group("Roulette Logic")
+@export_enum("none", "1st Column", "2nd Column", "3rd Column") var column: String = "none"
+@export_enum("none", "1st Dozen", "2nd Dozen", "3rd Dozen") var dozen: String = "none"
+@export_enum("none", "1-18 (Low)", "19-36 (High)") var range_half: String = "none"
+@export var is_even: bool = false
+
 var current_hover_zone: String = "none"
 
 func _ready():
-	mouse_entered.connect(_on_mouse_entered)
-	mouse_exited.connect(_on_mouse_exited)
+	var mesh_node = get_node("17_Mesh")
+	var mat = mesh_node.get_active_material(0)
+	
+	if mat:
+		# Create a unique copy of the material for THIS specific square
+		var unique_mat = mat.duplicate()
+		mesh_node.set_surface_override_material(0, unique_mat)
+		
+		if square_colour == "red":
+			unique_mat.albedo_color = Color("b61412")
+		elif square_colour == "black":
+			unique_mat.albedo_color = Color("000000")
 
 func _on_mouse_entered():
 	print("--- Mouse Entered Square: ", square_id, " ---")
@@ -42,8 +61,8 @@ func _input_event(camera, event, click_position, click_normal, shape_idx):
 	elif on_left: detected_zone = "edge_left"
 	elif on_top: detected_zone = "edge_top"
 	elif on_bottom: detected_zone = "edge_bottom"
-
-	# 3. Debug Print for Hovering
+	
+	
 	if event is InputEventMouseMotion:
 		if current_hover_zone != detected_zone:
 			current_hover_zone = detected_zone
